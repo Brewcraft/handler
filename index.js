76f4ae9b6920d75bb555c0808e2a052700fa48ce
SpreeHertz <spreehertzofficial@gmail.com>
2022-06-19 10:53:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const config = require('./config.json');
+const mineflayer = require('mineflayer');
+
+const prefix = config.chatPrefix || '!';
+
+const options = {
+    host: config.server_address || 'localhost',
+    port: parseInt(config.server_port) || '25565',
+    username: config.bot_username || 'herobrine'
+}
+
+const bot = mineflayer.createBot(options)
+
+const commandfiles = fs.readdirSync(__dirname + 'src/commands').filter(file => file.endsWith('.js'));
+let commands = [];
+
+for (const val of commandfiles) {
+    commands.push(val.replace('.js', ''))
+}
+
+bot.on('chat', (username, message) => {
+    if(!message.startsWith(prefix) || username === bot.username) return
+})
+
+const args = message.slice(prefix.length).trim().split(/ +/);
+
+const command = args.shift().toLowerCase();
+
+
+if (!commands.includes(command)) return;
+
+const torun = require(__dirname + 'src/commands/' + command + '.js')
+
+torun.execute(bot, args)
+
+bot.on('kicked', console.log)
+bot.on('error', console.log)
