0ac01761ea630558377638b060e9fb6abdfbd3fa
SpreeHertz <spreehertzofficial@gmail.com>
2022-06-19 11:10:08 +0530

    feat: add .eslintrc.json

---
+++ README.md
@@ -0,0 +1,12 @@
+# mineflayer-handler
+
+A simple handler for making [mineflayer](https://mineflayer.prismarine.js.org) bots.
+
+## Scripts
+
+`yarn dev`: Turn your bot on using nodemon (hot reloading package)
+`yarn lint`: View ESLint errors/warnings
+`yarn lint:fix`: Fix ESLint errors/warnings
+
+
+*This handler is a modified version of [somerandomcloud/minecraft-bot](https://github.com/somerandomcloud/minecraft-bot).*
--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const fs = require('fs')
+const fs = require('fs');
 const config = require('./config.json');
 const mineflayer = require('mineflayer');
 
 const prefix = config.chatPrefix || '!';
 
 const options = {
-    host: config.server_address || 'localhost',
-    port: parseInt(config.server_port) || '25565',
-    username: config.bot_username || 'herobrine'
-}
+	host: config.server_address || 'localhost',
+	port: parseInt(config.server_port) || '25565',
+	username: config.bot_username || 'herobrine',
+};
 
-const bot = mineflayer.createBot(options)
+const bot = mineflayer.createBot(options);
+const commandfiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
 
-const commandfiles = fs.readdirSync(__dirname + 'src/commands').filter(file => file.endsWith('.js'));
-let commands = [];
+const commands = [];
 
 for (const val of commandfiles) {
-    commands.push(val.replace('.js', ''))
+	commands.push(val.replace('.js', ''));
 }
 
 bot.on('chat', (username, message) => {
-    if(!message.startsWith(prefix) || username === bot.username) return
-})
+	if (!message.startsWith(prefix) || username === bot.username) return;
 
-const args = message.slice(prefix.length).trim().split(/ +/);
+	const args = message.slice(prefix.length).trim().split(/ +/);
 
-const command = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
 
+	if (!commands.includes(command)) return;
 
-if (!commands.includes(command)) return;
+	const torun = require(__dirname + '/commands/' + command + '.js');
 
-const torun = require(__dirname + 'src/commands/' + command + '.js')
+	torun.execute(bot, args);
+});
 
-torun.execute(bot, args)
-
-bot.on('kicked', console.log)
-bot.on('error', console.log)
+bot.on('kicked', console.log);
+bot.on('error', console.log);
